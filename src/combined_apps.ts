97c622ec58e66148c1fc630f500cdec6298317dc
Stephen Sisk <ssisk@users.noreply.github.com>
2022-07-24 13:08:38 -0600

    error message: mercato not accessed via valid domain

--- src/combined_apps.ts
+++ src/combined_apps.ts
@@ -11,5 +11,9 @@ export const getCombinedApp = async (app: Express, mercEnv: MercatoEnv) => {
   const stripPorts = (s: string) => s.split(":")[0];
   app.use(vhost("*." + stripPorts(domainNames[mercEnv]), filesApp));
   app.use(vhost(stripPorts(domainNames[mercEnv]), storeApp));
+  app.get("*", (req, res) => {
+    console.error("combinedApp: the request was not handled by one of the apps");
+    res.status(404).send("This is not a valid domain for this server").end();
+  });
   return app;
 };
