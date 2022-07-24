160119916dfb3330e497d5499e3fe614f1f41b72
Stephen Sisk <ssisk@users.noreply.github.com>
2022-07-24 13:42:45 -0600

    add logging to help debug domain names

--- src/combined_apps.ts
+++ src/combined_apps.ts
@@ -1,5 +1,6 @@
 import { Express } from "express";
 import vhost from "vhost";
+import { prodUrl } from "./environment";
 import { getAppFilesServer } from "./files_server";
 import { domainNames, MercatoEnv } from "./shared";
 import { getStoreServer } from "./store_server";
@@ -9,10 +10,18 @@ export const getCombinedApp = async (app: Express, mercEnv: MercatoEnv) => {
   const storeApp = await getStoreServer(mercEnv);
 
   const stripPorts = (s: string) => s.split(":")[0];
+  app.use(vhost("www." + stripPorts(domainNames[mercEnv]), storeApp));
   app.use(vhost("*." + stripPorts(domainNames[mercEnv]), filesApp));
   app.use(vhost(stripPorts(domainNames[mercEnv]), storeApp));
   app.get("*", (req, res) => {
-    console.error("combinedApp: the request was not handled by one of the apps");
+    console.error(
+      "combinedApp: the request was not handled by one of the apps. current domain name:",
+      domainNames[mercEnv],
+      "env:",
+      mercEnv,
+      "prod_url:",
+      prodUrl,
+    );
     res.status(404).send("This is not a valid domain for this server").end();
   });
   return app;
