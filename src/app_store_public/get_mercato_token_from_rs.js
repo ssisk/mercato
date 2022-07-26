83695cc3ae1567c4ddf13b402761cd7023819ea4
Stephen Sisk <ssisk@users.noreply.github.com>
2022-07-26 12:35:25 -0600

    allow token reset in case where token is not recognized by server This scenario is likely happening mostly on dev machines, but a mis-managed server upgrade *(eg where JWT secret is changed) could also trigger this

--- src/app_store_public/get_mercato_token_from_rs.js
+++ src/app_store_public/get_mercato_token_from_rs.js
@@ -21,6 +21,74 @@ const redirectAfterSuccess = () => {
   window.location.href = "/listing";
 };
 
+const resetMercatoToken = async (client, existingTokenFilename, existingTokenFileContents) => {
+  console.log("Starting mercato token reset");
+  // crypto.randomUUID doesn't exist on non-https pages (aka dev machines)
+  let randomId = crypto.randomUUID ? crypto.randomUUID() : Math.random() * 10000;
+  await writeMercatoTokenToFile(
+    client,
+    existingTokenFilename + "-recovery-" + randomId,
+    existingTokenFileContents,
+  );
+  await client.remove(existingTokenFilename);
+  alert("Successfully reset token - refresh this page to log in to this server.");
+};
+
+const writeMercatoTokenToFile = async (client, filename, token) => {
+  await client.storeFile("text/plain", filename, token);
+  console.log("wrote token", token, "to file", filename);
+};
+
+const validateAccount = async (mercatoToken, serverUrl, client, tokenFilename) => {
+  const rawRes = await fetch("/user/validate", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ mercatoToken, serverUrl }),
+  });
+  if (rawRes.status != 200) {
+    document.getElementById("resetToken").style.display = "block";
+    document.getElementById("doTokenReset").onclick = async () => {
+      await resetMercatoToken(client, tokenFilename, mercatoToken).finally("finished resetMercatoToken");
+    };
+    return;
+  }
+  const res = JSON.parse(await rawRes.text());
+  if (res.success) {
+    console.log("success!");
+    alert("You are now logged in to this Mercato sever on this device! You can now use the apps on this server.");
+    redirectAfterSuccess();
+  } else {
+    alert("Sorry, login did not work! Please try again");
+  }
+};
+
+const createAccount = async (serverUrl, client, filename) => {
+  const rawRes = await fetch("/user/create", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ serverUrl }),
+  });
+  if (rawRes.status != 200) {
+    alert(
+      "There was an error trying to create an account for you on this server, please try again. If further errors occur, please contact the admin of this server.",
+    );
+    console.error("Error from server was: ", rawRes);
+    return;
+  }
+  const res = JSON.parse(await rawRes.text());
+  if (!res.success) {
+    // this shouldn't happen? server 200 is supposed to be correct
+    alert("Unexpected error creating an account for you - please retry.");
+    console.error();
+    throw new Error("/user/create error. response was: " + res);
+  }
+  const token = res.token;
+  console.log("received token from server: ", token);
+  await writeMercatoTokenToFile(client, filename, token);
+  alert("You now have an account on this Mercato sever! You can now use the apps on this server.");
+  redirectAfterSuccess();
+};
+
 const rsInit = () => {
   if (!__rs) {
     console.log("Initing RS");
@@ -35,48 +103,14 @@ const rsInit = () => {
       // Storage account has been connected, let’s roll!
       console.log("RS connected");
       const client = rs.scope("/mercato/");
-      const file = await client.getFile(mercatoTokenFilename());
+      const tokenFilename = mercatoTokenFilename();
+      const file = await client.getFile(tokenFilename);
       if (file.data) {
-        // todo: can we validate the user's server matches that in the token??
-        const rawRes = await fetch("/user/validate", {
-          method: "post",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ mercatoToken: file.data, serverUrl }),
-        });
-        if (rawRes.status != 200) {
-          alert(
-            "You have a mercato token in your RemoteStorage, but this server did not recognize it! "
-              + "Please contact the admins of your Mercato server (the domain you're on right now).",
-          );
-          return;
-        }
-        const res = JSON.parse(await rawRes.text());
-        if (res.success) {
-          console.log("success!");
-          alert("You are now logged in to this Mercato sever on this device! You can now use the apps on this server.");
-          redirectAfterSuccess();
-        } else {
-          alert("Sorry, login did not work! Please try again");
-        }
+        validateAccount(file.data, serverUrl, client, tokenFilename).finally(() =>
+          console.log("validate account finished")
+        );
       } else {
-        const rawRes = await fetch("/user/create", {
-          method: "post",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ serverUrl }),
-        });
-        if (rawRes.status != 200) {
-          alert(
-            "There was an error trying to create an account for you on this server, please try again. If further errors occur, please contact the admin of this server.",
-          );
-          console.error("Error from server was: ", rawRes);
-          return;
-        }
-        const res = JSON.parse(await rawRes.text());
-        if (res.success) {
-          await client.storeFile("text/plain", mercatoTokenFilename(), res.token);
-          alert("You now have an account on this Mercato sever! You can now use the apps on this server.");
-          redirectAfterSuccess();
-        }
+        createAccount(serverUrl, client, tokenFilename).finally(() => console.log("validate account finished"));
       }
     });
     rs.on("not-connected", () => {
